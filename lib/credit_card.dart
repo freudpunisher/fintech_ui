c22d43bd0c2d707254c998efbad7431f9689ece1
freudpunisher <punishergte@gmail.com>
2024-09-18 16:49:20 +0200

    first commit

---
+++ lib/credit_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CreditCard extends StatelessWidget {
+  const CreditCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 228,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: const Color.fromARGB(255, 14, 19, 29),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: 16,
+                          left: 16,
+                          child: Image.asset(
+                            "assets/credit-card.png",
+                            height: 40,
+                            color: Colors.white,
+                          )),
+                      Positioned(
+                          top: 10,
+                          left: 70,
+                          child: Image.asset(
+                            "assets/wifi.png",
+                            height: 50,
+                            color: Colors.white,
+                          )),
+                      Positioned(
+                          bottom: 16,
+                          left: 16,
+                          child: Text(
+                            "*** **** **** 1854",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
