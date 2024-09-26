a13258bde047ed4d5f376fdd983361fee6a76b41
freudpunisher <punishergte@gmail.com>
2024-09-26 09:09:22 +0200

    "Refactored ActivityPage widget: updated imports, modified AppBar, rearranged and reformatted code, added new UI elements and styling."

--- lib/pages/activity.dart
+++ lib/pages/activity.dart
@@ -1,7 +1,7 @@
-import 'package:fintech_ui/widgets/time_option_button.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+import '../widgets/time_option_button.dart';
 
 class ActivityPage extends StatelessWidget {
   const ActivityPage({super.key});
@@ -14,17 +14,23 @@ class ActivityPage extends StatelessWidget {
         backgroundColor: Colors.white,
         leading: IconButton.outlined(
           onPressed: () {},
-          icon: const Icon(Icons.arrow_back_ios_new),
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+          ),
         ),
-        title: const Center(
-          child: Text(
-            "Activity",
-            style: TextStyle(fontWeight: FontWeight.w500),
+        title: const Text(
+          "Activity",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
           ),
         ),
         actions: [
           IconButton.outlined(
-              onPressed: () {}, icon: const Icon(Icons.more_horiz))
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_horiz,
+            ),
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -36,109 +42,116 @@ class ActivityPage extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: List.generate(
-                      3,
-                      (index) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              width: 340,
-                              height: 75,
-                              decoration: BoxDecoration(
-                                color: index % 2 == 0
-                                    ? const Color.fromARGB(2555, 16, 80, 98)
-                                    : Colors.black,
-                                borderRadius: BorderRadius.circular(20),
+                    3,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 340,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          color: (index % 2 == 0)
+                              ? const Color.fromARGB(255, 16, 80, 98)
+                              : Colors.black,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Text(
+                                "Smartpay Cards",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600),
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
+                              const Spacer(),
+                              const Text(
+                                "**** 1990",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 12.0),
                                 child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
-                                    const Text(
-                                      "Smartphone",
-                                      style: TextStyle(
-                                          fontSize: 18, color: Colors.white),
+                                    CircleAvatar(
+                                      radius: 15,
+                                      backgroundColor:
+                                          Colors.white.withOpacity(0.8),
                                     ),
-                                    const Spacer(),
-                                    const Text(
-                                      "9/13",
-                                      style: TextStyle(
-                                          fontSize: 14, color: Colors.grey),
-                                    ),
-                                    const SizedBox(
-                                      width: 8,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        CircleAvatar(
-                                          radius: 15,
-                                          backgroundColor:
-                                              Colors.white.withOpacity(0.8),
-                                        ),
-                                        Transform.translate(
-                                            offset: const Offset(-10, 0),
-                                            child: CircleAvatar(
-                                              radius: 15,
-                                              backgroundColor:
-                                                  Colors.white.withOpacity(0.8),
-                                            )),
-                                      ],
-                                    ),
+                                    Transform.translate(
+                                      offset: const Offset(-10, 0),
+                                      child: CircleAvatar(
+                                        radius: 15,
+                                        backgroundColor:
+                                            Colors.white.withOpacity(0.8),
+                                      ),
+                                    )
                                   ],
                                 ),
                               ),
-                            ),
-                          )),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 25,
-              ),
+              const SizedBox(height: 25),
               Container(
+                padding: EdgeInsets.all(12),
                 width: double.maxFinite,
                 height: 350,
                 decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey[300]!,
-                  ),
+                  border: Border.all(color: Colors.grey[300]!),
                   borderRadius: BorderRadius.circular(17),
                 ),
                 child: Column(
                   children: [
                     const Text(
-                      "Total Spending Today",
+                      "Total Spending",
                       style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
                     ),
-                    const SizedBox(
-                      height: 4,
-                    ),
+                    const SizedBox(height: 4),
                     const Text(
-                      "\$ 45.99",
+                      "\$6,345.00",
                       style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    const SizedBox(
-                      height: 12,
-                    ),
+                    const SizedBox(height: 12),
                     const TimeOptionRow(),
                     const SizedBox(height: 16),
                     Expanded(
-                        child: LineChart(
-                      LineChartData(
-                          gridData: const FlGridData(show: false),
+                      child: LineChart(
+                        LineChartData(
+                          gridData: FlGridData(show: false),
                           titlesData: FlTitlesData(
-                            leftTitles: const AxisTitles(
-                                sideTitles: SideTitles(showTitles: false)),
+                            leftTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
                             bottomTitles: AxisTitles(
                               sideTitles: SideTitles(
                                   showTitles: true,
                                   getTitlesWidget: (value, meta) {
-                                    const tiles = ['S', 'M', 'T', 'W'];
+                                    const tiles = [
+                                      'S',
+                                      'M',
+                                      'T',
+                                      'W',
+                                      'T',
+                                      'F'
+                                    ];
                                     final index = value.toInt();
                                     if (index >= 0 && index < tiles.length) {
                                       return Text(
@@ -153,13 +166,11 @@ class ActivityPage extends StatelessWidget {
                                   interval: 1),
                             ),
                             rightTitles: const AxisTitles(
-                                sideTitles: SideTitles(
-                              showTitles: false,
-                            )),
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
                             topTitles: const AxisTitles(
-                                sideTitles: SideTitles(
-                              showTitles: false,
-                            )),
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
                           ),
                           borderData: FlBorderData(show: false),
                           lineBarsData: [
@@ -176,10 +187,68 @@ class ActivityPage extends StatelessWidget {
                                 color: Colors.teal,
                                 barWidth: 3,
                                 dotData: const FlDotData(show: false),
-                                belowBarData: BarAreaData(show: true)),
-                          ]),
-                    ))
+                                belowBarData: BarAreaData(
+                                    show: true,
+                                    color: Colors.teal.withOpacity(0.07)))
+                          ],
+                        ),
+                      ),
+                    )
                   ],
+                ),
+              ),
+              const SizedBox(height: 25),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Transaction",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "All",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.teal,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: List.generate(
+                  3,
+                  (index) => const ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 239, 243, 245),
+                      child: Icon(
+                        Icons.payments_rounded,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    title: Text(
+                      "Smartpay UI Kit",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    subtitle: Text("ui8.net"),
+                    trailing: Text(
+                      "-\$45.99",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+                    ),
+                  ),
                 ),
               )
             ],
